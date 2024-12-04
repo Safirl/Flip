4e8ceb25b943bd73b2dbfb91ca47b71e5bc2e6f4
coraliealexandru <coralie.alexandru@4sh.fr>
2024-12-04 16:40:25 +0100

    init account design

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -26,18 +26,4 @@
         @yield('content')
 </div>
 </body>
-<style>
-    .alert {
-        padding: 0.75rem 1.25rem;
-        margin-bottom: 1rem;
-        border: 1px solid transparent;
-        border-radius: 0.25rem;
-    }
-
-    .alert-success {
-        color: #0f5132;
-        background-color: #d1e7dd;
-        border-color: #badbcc;
-    }
-</style>
 </html>
