342b4374ef8496e817a063b3469efdc0259e6918
coraliealexandru <coralie.alexandru@4sh.fr>
2024-12-04 12:12:53 +0100

    connexion design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,8 +17,29 @@
                     <input type="password" class="form-control" id="password" name="password">
                     @error("password") {{ $message }} @enderror
                 </div>
-                <button class="btn btn-primary">Se connecter</button>
+                <x-button
+                    size="large"
+                    color="primary"
+                    kind="fill"
+                    label="Connexion"
+                    expand="true"
+                    iconEnd="fa-regular fa-chevron-left"
+                />
+            </form>
+        </div>
+        <div class="card-body">
+            <p>Inscris toi pour pouvoir voir les votes de tes amis</p>
+            <form action="{{route('auth.register.show')}}" method="get">
+                @csrf
+                <x-button
+                    size="large"
+                    color="primary"
+                    outlined="true"
+                    label="Inscription"
+                    expand="true"
+                />
             </form>
         </div>
     </div>
+
 @endsection
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,21 +9,19 @@
     <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/components/_button.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 
-    {{--    <!-- --}}{{--Bootstrap Remove later--}}{{-- -->--}}
-    {{--  <!--  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"--}}
-    {{--          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">-->--}}
 </head>
 <body>
 
 <div class="container">
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{session('success')}}
-        </div>
-    @endif
-    @yield('content')
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{session('success')}}
+            </div>
+        @endif
+        @yield('content')
 </div>
 
 </body>
