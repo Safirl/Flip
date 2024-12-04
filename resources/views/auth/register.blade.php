f69b5c3464632341278e481a02706621f98ac53e
coraliealexandru <coralie.alexandru@4sh.fr>
2024-12-04 12:49:51 +0100

    init register design

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,32 +1,36 @@
 @extends('base')
 @section('title', 'Login')
-
 @section('content')
     <h1>Créer un compte</h1>
     <div class="card">
-        <div class="card-body">
-            <form action="{{route('auth.register.create')}}" method="post" class="vstack gap-3">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Pseudo</label>
-                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
-                    @error("name") {{ $message }} @enderror
-                </div>
+        <form action="{{route('auth.register.create')}}" method="post" class="vstack gap-3">
+            @csrf
+            <div class="form-group">
+                <label for="name">Pseudo</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+                @error("name") {{ $message }} @enderror
+            </div>
 
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" value= {{ old('email') }}>
-                    @error("email") {{ $message }} @enderror
-                </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" value= {{ old('email') }}>
+                @error("email") {{ $message }} @enderror
+            </div>
 
-                <div class="form-group">
-                    <label for="password">Mot de passe</label>
-                    <input type="password" class="form-control" id="password" name="password">
-                    @error("password") {{ $message }} @enderror
-                </div>
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" class="form-control" id="password" name="password">
+                @error("password") {{ $message }} @enderror
+            </div>
 
-                <button class="btn btn-primary">Se connecter</button>
-            </form>
-        </div>
+            <x-button
+                size="large"
+                color="primary"
+                outlined="true"
+                label="Inscription"
+                expand="true"
+            />
+        </form>
     </div>
+
 @endsection
