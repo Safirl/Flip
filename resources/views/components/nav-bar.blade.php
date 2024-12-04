21eb9f9bc28069c8823a0f36e75ce40c4a590c75
coraliealexandru <coralie.alexandru@4sh.fr>
2024-12-04 18:16:35 +0100

    fix

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar">
-    <x-nav-button icon="fa-regular fa-home" label="Accueil" url='polls'/>
+    <x-nav-button icon="fa-solid fa-home" label="Accueil" url='polls'/>
     <x-nav-button icon="fa-solid fa-square-poll-vertical" label="Résultats" url='feed'/>
     <x-nav-button icon="fa-solid fa-user" label="Profil" url='account'/>
 </div>
