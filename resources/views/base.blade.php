c875de32cec30b43538db0f470faca00b72d838e
coraliealexandru <coralie.alexandru@4sh.fr>
2024-12-05 12:47:27 +0100

    init design results

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="/logo-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/logo-16x16.png">
-    <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/logo-180x180.png">
+    <link rel="icon" sizes="32x32" href="../../public/images/logo-32x32.png">
+    <link rel="icon" sizes="16x16" href="../../public/images/logo-16x16.png">
+    <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="../../public/images/logo-180x180.png">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
