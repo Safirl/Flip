8b2d8d02f46ff6f18e399d22ba7037eaf373841a
coraliealexandru <coralie.alexandru@4sh.fr>
2024-12-05 10:07:35 +0100

    feed fix style

--- resources/views/app/polls.blade.php
+++ resources/views/app/polls.blade.php
@@ -1,10 +1,9 @@
-
 @extends('base')
 
 @section('title', 'Votes du jour')
 @section('styles')
     <link rel="stylesheet" href="{{ asset('css/polls.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 @endsection
 
 @section('content')
@@ -14,29 +13,37 @@
         <div class="swiper-wrapper">
             @foreach ($polls as $poll)
                 <div class="swiper-slide">
-                    <h2>{{ $poll->title }}</h2>
-                    <p><strong>Quote:</strong> "{{ $poll->quote }}"</p>
-                    <p><strong>Author:</strong> {{ $poll->author }}</p>
-                    <p><strong>Context:</strong> {{ $poll->context }}</p>
-                    <p><strong>Analysis:</strong> {{ $poll->analysis }}</p>
-                    <p><strong>Slug:</strong> {{ $poll->slug }}</p>
+                    <h4>{{ $poll->author }}</strong> :</h4>
+                    <p class="author"><em>"{{ $poll->quote }}"</em></p>
+                    {{-- <p><strong>Context:</strong> {{ $poll->context }}</p> --}}
+                    {{--<p><strong>Analysis:</strong> {{ $poll->analysis }}</p>--}}
 
-            @if (
-                (session()->has('completed_polls') && in_array($poll->id, session('completed_polls'))) ||
-                auth()->check() && $poll->isVoteVerification
-                )
-                <a href="{{ route('app.result', ['poll' => $poll->slug]) }}">Lire la suite</a>
-            @else
-                <form action="{{ route('app.result', ['poll' => $poll->slug]) }}" method="POST">
-                    @csrf
-                    <input type="radio" id="intox-{{ $poll->id }}" name="answer" value="false" required>
-                    <label for="intox-{{ $poll->id }}">Intox</label>
-
-                    <input type="radio" id="info-{{ $poll->id }}" name="answer" value="true" required>
-                    <label for="info-{{ $poll->id }}">Info</label>
-                    <button type="submit" class="btn btn-primary">Valider</button>
-                </form>
-            @endif
+                    @if (session()->has('completed_polls') && in_array($poll->id, session('completed_polls')) ||
+                         (auth()->check() && $poll->users()->exists()))
+                        <a href="{{ route('app.result', ['poll' => $poll->slug]) }}">Lire la suite</a>
+                    @else
+                        <form class="form" action="{{ route('app.result', ['poll' => $poll->slug]) }}" method="POST">
+                            @csrf
+                            <div class="form-buttons">
+                                <div class="buttons-item">
+                                    <input type="radio" id="intox-{{ $poll->id }}" name="answer" value="false">
+                                    <label for="intox-{{ $poll->id }}">Intox</label>
+                                </div>
+                                <div class="buttons-item">
+                                    <input type="radio" id="info-{{ $poll->id }}" name="answer" value="true">
+                                    <label for="info-{{ $poll->id }}">Info</label>
+                                </div>
+                            </div>
+                            <x-button id="submit-button"
+                                      type="submit"
+                                      size="large"
+                                      color="primary"
+                                      label="Valider"
+                                      iconStart="fa-solid fa-check"
+                                      class="hidden"
+                            />
+                        </form>
+                    @endif
                 </div>
             @endforeach
     </div>
@@ -49,8 +56,10 @@
             effect: "cards",
             grabCursor: true,
         });
+
     </script>
 
 @endsection
 @section('scripts')
+
 @endsection
