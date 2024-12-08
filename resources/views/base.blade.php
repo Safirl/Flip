2efee02d1e089be33a031288c471529a9588f4da
Safirl <loic.leforestier@mmibordeaux.com>
2024-12-08 22:07:29 +0100

    moving css in resources and adding vite

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="icon" sizes="16x16" href="../../public/favicon.ico">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components/_button.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components/_nav.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components/_link.css') }}">
-    @yield('styles')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-
+{{--    <link rel="icon" sizes="16x16" href="../../public/favicon.ico">--}}
+{{--    <link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
+{{--    <link rel="stylesheet" href="{{ asset('css/variables.css') }}">--}}
+{{--    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">--}}
+{{--    <link rel="stylesheet" href="{{ asset('css/components/_button.css') }}">--}}
+{{--    <link rel="stylesheet" href="{{ asset('css/components/_nav.css') }}">--}}
+{{--    <link rel="stylesheet" href="{{ asset('css/components/_link.css') }}">--}}
+{{--    @yield('styles')--}}
+{{--    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">--}}
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
