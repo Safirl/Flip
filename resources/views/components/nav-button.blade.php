df3c9e0727f0bbcb311d3b1ba20383643bc649d6
Safirl <loic.leforestier@mmibordeaux.com>
2025-01-05 19:05:23 +0100

    adding middle space

--- resources/views/components/nav-button.blade.php
+++ resources/views/components/nav-button.blade.php
@@ -6,5 +6,4 @@
 <a href="{{ route($url) }}"
    class="nav-button {{ $isActive ? 'active' : '' }}">
     <img class="icon" alt="{{ $label }} icon" src="{{ asset('images/icons/'. $icon .($isActive ? '-active' : '') .'.svg') }}">
-    <div class="nav-indicator"></div>
 </a>
