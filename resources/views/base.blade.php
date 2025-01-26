0d3164f2cd8e4bac3f50a2e2cc1afc5a84ac7131
Arthaud <arthaudp33@gmail.com>
2025-01-26 12:44:23 +0100

    :bento: Add barcode 39 font

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+39&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -18,7 +19,7 @@
         </div>
     @endif
     @yield('content')
-        <x-nav-bar/>
+    <x-nav-bar/>
 </div>
 </body>
 </html>
