553b2eabb57501ba43d56d2da88409b7e6399cbd
Arthaud <arthaudp33@gmail.com>
2025-02-03 18:40:58 +0100

    :sparkles: Account page

--- resources/views/app/account.blade.php
+++ resources/views/app/account.blade.php
@@ -1,148 +1,104 @@
-@vite(['resources/css/account.css'])
+@vite(['resources/css/account.scss'])
 
-<x-layouts.base-with-nav title="Profil">
-    <div class="section-user-profile">
-        <div class="user-profile">
-            <div class="title-user-profile">
-                <h1>MON COMPTE</h1>
-                @guest()
-                    <p>
-                        Vous n’êtes pas connecté
-                    </p>
-                @endguest
-                @auth()
-                    <p>{{ \Illuminate\Support\Facades\Auth::user()->name }}</p>
-                    <p>Votre code ami : <strong>{{ \Illuminate\Support\Facades\Auth::user()->friend_id }}</strong></p>
-                @endauth
-            </div>
-            <div class="disconnect-btn-container">
-                @auth()
+<x-layouts.base-with-nav title="Mon compte">
+    <header>
+        <h1>Mon compte</h1>
 
-            </div>
-            @can('doAdminActions')
-                <div>
+        @isset($user)
+            <p>{{ $user->name }}</p>
+            <p>Votre code ami : <strong>{{ $user->friend_id }}</strong></p>
 
-                    <form action="{{ route('admin.index') }}" method="get">
-                        @csrf
-                        <x-button
-                            size="small"
-                            type="submit"
-                            color="primary"
-                            label="Voir tous les polls"
-                            expand="true"
-                            kind="clear"
-                        />
-                    </form>
-                </div>
-                <div>
-                    <form action="{{ route('admin.create.poll') }}" method="get">
-                        @csrf
-                        <x-button
-                            size="small"
-                            type="submit"
-                            color="primary"
-                            label="Créer un poll"
-                            expand="true"
-                            kind="clear"
-                        />
-                    </form>
-                </div>
-            @endif
-            @endauth
-            <div class="friend-container">
-                @auth
-                    <div class="card user-card">
-                        <div class="card-body">
-                            <p>Rechercher un ami pour voir ses votes et voir ses commentaires</p>
-                            <form action="{{ route('addFriend') }}" method="post" enctype="multipart/form-data"
-                                  class="add-friend-form">
-                                @csrf
-                                <div class="contenaire-form">
-                                    <label for="friend_id"><p><strong>Saisissez le code ami</strong></p></label>
-                                    <input type="text" class="form-control" id="friend_id" name="friend_id">
-                                    @error("friend_id") <span class="text-error">{{ $message }}</span> @enderror
-                                </div>
-                                <x-button
-                                    size="large"
-                                    color="primary"
-                                    label="Valider"
-                                    expand="true"
-                                />
-                            </form>
-                        </div>
-                    </div>
-                    {{--Show friends--}}
-                    <div class="flex">
-                        <p class="friends-p"><strong>Vos amis </strong>({{count($friends)}}) :</p>
-                        @foreach($friends as $friend)
-                            <div class="friend-card card">
-                                <div class="friend-content">
-                                    <p style="font-weight: normal">{{ $friend->name }}</p>
-                                    <p style="font-weight: lighter; font-size: 10px">{{ $friend->friend_id }}</p>
-                                    <div class="decoration-contenaire-friend"></div>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                @endauth
-                @guest
-                    <div class="card delete-btn">
-                        <p class="card-account-text">
-                            Vous souhaitez partager l’expérience avec vos proches ? Créez-vous un compte pour partager
-                            votre
-                            code ami.
-                        </p>
-                        <form action="{{route('auth.login')}}" method="get">
-                            @csrf
-                            <x-button
-                                size="large"
-                                color="primary"
-                                label="Connexion / Inscription"
-                                expand="true"
-                            />
-                        </form>
-                    </div>
-                @endguest
-            </div>
+            <form action="{{ route('auth.logout') }}" method="post">
+                @method("delete")
+                @csrf
+                <button>Me déconnecter</button>
+            </form>
+        @else
+            <p>
+                Vous n’êtes pas connecté
+            </p>
+        @endif
+    </header>
+
+    @can('doAdminActions')
+        <div class="admin-actions">
+            <h2>Actions admin</h2>
+
+            <a href="{{ route('admin.index') }}">
+                Voir tous les polls
+            </a>
+
+            <a href="{{ route('admin.create.poll') }}">
+                Créer un poll
+            </a>
         </div>
-        <div class="end-link">
-            <x-link color="secondary"
-                    size="medium"
-                    expand="true"
-                    noPadding=true
-                    url='mentionslegales'
-                    label="Voir les mentions légales">
-            </x-link>
-            @auth
-                <form action="{{ route('auth.logout') }}" method="post">
-                    @method("delete")
-                    @csrf
-                    <x-button
-                        size="small"
-                        type="submit"
-                        color="secondary"
-                        label="Se déconnecter"
-                        expand="true"
-                        kind="clear"
+    @endif
 
-                    />
-                </form>
-                <div class="delete-btn">
-                    <form class="disconnect-btn"
-                          action="{{ route('auth.destroy', \Illuminate\Support\Facades\Auth::user()) }}" method="post">
-                        @method("delete")
-                        @csrf
-                        <x-button
-                            kind="clear"
-                            type="submit"
-                            size="small"
-                            color="secondary"
-                            label="Supprimer le compte"
-                            expand="true"
-                        />
-                    </form>
+    @isset($user)
+        <section>
+            <p>Ajoutez un ami pour voir ses votes et voir ses commentaires</p>
+            <form action="{{ route('addFriend') }}" method="post">
+                @csrf
+
+                <div>
+                    <label for="friend_id">Saisissez un code ami</label>
+                    <input type="text" id="friend_id" name="friend_id">
+                    @error("friend_id")
+                    <p class="text-error">{{ $message }}</p>
+                    @enderror
                 </div>
-        </div>
-        @endauth
-    </div>
+
+                <button>Valider</button>
+            </form>
+        </section>
+
+        <section class="friends">
+            <h2>Vos amis ({{ count($friends) }}) :</h2>
+            <ul>
+                @forelse($friends as $friend)
+                    <li>
+                        <p>{{ $friend->name }}</p>
+                        <p>Code ami: #{{ $friend->friend_id }}</p>
+                    </li>
+                @empty
+                    <li>
+                        <p>Vous n'avez pas ajouté d'ami</p>
+                        <p>Ajoutez des amis via leur code !</p>
+                    </li>
+                @endforelse
+
+            </ul>
+        </section>
+    @else
+        <section>
+            <p>
+                Vous souhaitez partager l’expérience avec vos proches ?
+                Créez-vous un compte pour partager votre code ami.
+            </p>
+
+            <a href="{{route('auth.login')}}">
+                Connexion / Inscription
+            </a>
+        </section>
+    @endif
+
+    <nav>
+        <a href="{{route('mentionslegales')}}">
+            Mentions Légales
+        </a>
+
+        @isset($user)
+            <form
+                action="{{ route('auth.destroy', $user) }}"
+                method="post"
+            >
+                @method("delete")
+                @csrf
+
+                <button>
+                    Supprimer le compte
+                </button>
+            </form>
+        @endif
+    </nav>
 </x-layouts.base-with-nav>
