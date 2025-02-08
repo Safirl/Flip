5b2e010f1360cbfdd97de8a0227598cb7763d93b
Arthaud <arthaudp33@gmail.com>
2025-02-08 23:26:29 +0100

    :bento: Add favicon & pwa manifest
    Favicons/manifest/html made with https://realfavicongenerator.net/

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -8,6 +8,15 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+39&display=swap" rel="stylesheet">
+
+    <!-- made on https://realfavicongenerator.net/ -->
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"/>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <meta name="apple-mobile-web-app-title" content="Smash"/>
+    <link rel="manifest" href="/site.webmanifest"/>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -17,6 +26,7 @@
         <div class="alert alert-success">
             {{session('success')}}
         </div>
+
     @endif
 
     {{ $slot }}
